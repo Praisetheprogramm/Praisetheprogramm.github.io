97ccac1a6cafdd2ac05ee8edd98de5326ac4af4f
Praisetheprogramm <rick07klapschuweit@gmail.com>
2025-03-17 14:25:20 +0100

    .

---
+++ arkiv/api-lek/kode.js
@@ -0,0 +1,14 @@
+let overskrift = document.createElement("h1");
+overskrift.innerText = "Dette er en overskrift.";
+document.body.appendChild(overskrift);
+
+
+
+let paragraf = document.createElement("p");
+paragraf.innerText = "Dette er en paragraf.";
+document.body.appendChild(paragraf);
+
+
+let bilde = document.createElement("img");
+bilde.src = "retro-kassetten-spieler-groovy-maskottchen_147671-1654.avif";
+document.body.appendChild(bilde);   
