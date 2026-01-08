d361b24e42710b94afa918623469436f3fc5d8c3
Praisetheprogramm <rick07klapschuweit@gmail.com>
2026-01-08 10:55:10 +0100

    fef

--- kode.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', function() {
-    const gallery = document.getElementById('gallery');
-    const scrollPosition = window.pageYOffset;
-    gallery.style.transform = `translateY(${-scrollPosition}px)`;
-});
